39e0d34340ef026fb1fb9ee07abbca51a3acad8a
Jeans <jrayland@amazon.com>
2017-11-02 09:00:59 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 cp tmux/tmux.conf ~/.tmux.conf
 cp vim/vimrc ~/.vimrc
+cp bash/bash_aliases ~/.bash_aliases
 
 if [ ! -d ~/.vim ]; then
     mkdir -p ~/.vim
@@ -14,4 +15,5 @@ fi
 if [ ! -d ~/.vim/autoload/plug.vim ]; then
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    vim +PlugInstall +qall
 fi
